8f76237017ae47cf04fc5b6a0c06b1dacc820b43
domus_desarrollo2 <desarrollo2@domus.la>
2025-03-28 12:49:09 -0500

    disenio final

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -3,51 +3,96 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> {{ $tittle}} | ByteRest</title>
-    <!-- Favicon icon -->
-    <link rel="icon"  href="{{ asset('assets/img/logo.svg') }}"  type="image/x-icon">
+    <title>Sistema POS - Sidebar & Navbar Inferior</title>
 
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet" type="text/css" />
 </head>
-
 <body class="bg-gray-100">
-	<meta name="csrf-token" content="{{ csrf_token() }}">
 
-	<div class="flex h-screen">
-        <!-- Sidebar -->
-        <div class="group flex flex-col items-center w-16 hover:w-64 transition-all duration-300 bg-gray-600 text-white shadow-lg">
-            <!-- Header -->
-            <div class="flex items-center justify-between w-full p-4">
-                <span class="text-xl font-bold hidden group-hover:block">Menú</span>
-                <label for="sidebar-toggle" class="btn btn-circle text-white bg-transparent border-none lg:hidden">☰</label>
+    <div class="flex h-screen">
+        <!-- Sidebar pequeño -->
+        <aside class="sidebar-small hidden lg:flex rounded-tr-3xl rounded-br-3xl" id="sidebarSmall">
+            <div class="text-center">
+                <ul class="menu menu-lg rounded-box">
+                    <li class="p-2">
+                        <button onclick="toggleSidebar('sidebarLarge')" class="p-2 transition-colors rounded-lg shadow-md hover:bg-indigo-800 hover:text-white focus:outline-none focus:ring focus:ring-indigo-600 focus:ring-offset-white focus:ring-offset-2 text-gray-500 bg-white">
+                        <i class="material-icons text-3xl">view_list</i>
+                    </li>
+                    <li class="p-2">
+                        <button onclick="toggleSidebar('sidebarMessages')" class="p-2 transition-colors rounded-lg shadow-md hover:bg-indigo-800 hover:text-white focus:outline-none focus:ring focus:ring-indigo-600 focus:ring-offset-white focus:ring-offset-2 text-gray-500 bg-white">
+                            <i class="material-icons text-3xl">sms</i>
+                        </button>
+                    </li>
+                    <li class="p-2">
+                        <button onclick="toggleSidebar('sidebarNotifications')" class="p-2 transition-colors rounded-lg shadow-md hover:bg-indigo-800 hover:text-white focus:outline-none focus:ring focus:ring-indigo-600 focus:ring-offset-white focus:ring-offset-2 text-gray-500 bg-white">
+                            <i class="material-icons text-3xl">notifications</i>
+                        </button>
+                    </li>
+                    <li class="p-2">
+                        <button onclick="toggleSidebar('sidebarSettings')" class="p-2 transition-colors rounded-lg shadow-md hover:bg-indigo-800 hover:text-white focus:outline-none focus:ring focus:ring-indigo-600 focus:ring-offset-white focus:ring-offset-2 text-gray-500 bg-white">
+                            <i class="material-icons text-3xl">settings</i>
+                        </button>
+                    </li>
+                </ul>
+                
             </div>
+        </aside>
+        
+        <!-- Sidebar grande -->
+        <aside class="sidebar-large rounded-tr-3xl rounded-br-3xl" id="sidebarLarge">
+            <nav class="mt-5">
+                <a href="/dashboard" class="flex items-center space-x-2 text-indigo-600 transition-colors rounded-lg group hover:bg-indigo-600 hover:text-white">
+                    <span aria-hidden="true" class="p-2 transition-colors rounded-lg group-hover:bg-indigo-700 group-hover:text-white"><i class="material-icons text-3xl">home</i></span>
+                    <span>Inicio</span>
+                </a>
+                <a href="/product" class="flex items-center space-x-2 text-indigo-600 transition-colors rounded-lg group hover:bg-indigo-600 hover:text-white">
+                    <span aria-hidden="true" class="p-2 transition-colors rounded-lg group-hover:bg-indigo-700 group-hover:text-white"><i class="material-icons text-3xl">inventory_2</i></span>
+                    <span>Productos</span>
+                </a>
+                <a href="/product" class="flex items-center space-x-2 text-indigo-600 transition-colors rounded-lg group hover:bg-indigo-600 hover:text-white">
+                    <span aria-hidden="true" class="p-2 transition-colors rounded-lg group-hover:bg-indigo-700 group-hover:text-white"><i class="material-icons text-3xl">fact_check</i></span>
+                    <span>Facturación</span>
+                </a>
+            </nav>
+        </aside>
 
-            <!-- Usuario -->
-            <div class="flex flex-col items-center group-hover:flex-row gap-2 p-4">
-                <img src="https://i.pravatar.cc/40" class="rounded-full border-2 border-white">
-                <span class="hidden group-hover:block">{{ session('user_name') }}</span>
-            </div>
+        <!-- Sidebar de mensajes -->
+        <aside class="sidebar-messages rounded-tr-3xl rounded-br-3xl" id="sidebarMessages">
+            <nav class="mt-5">
+                <p class="p-2 text-gray-600">📩 Mensajes</p>
+            </nav>
+        </aside>
 
-            <!-- Menú -->
-            <ul class="menu p-2 flex-1 w-full">
+        <!-- Sidebar de notificaciones -->
+        <aside class="sidebar-notifications rounded-tr-3xl rounded-br-3xl" id="sidebarNotifications">
+            <nav class="mt-5">
+                <p class="p-2 text-gray-600">🔔 Notificaciones</p>
+            </nav>
+        </aside>
+
+        <!-- Sidebar de setting -->
+        <aside class="sidebar-settings rounded-tr-3xl rounded-br-3xl" id="sidebarSettings">
+            <nav class="mt-5">
+                <p class="p-2 text-gray-600">🔔 Configuracion</p>
+                <ul class="menu bg-base-200 rounded-box w-56">
                 <li>
-                    <a href="/dashboard" class="flex items-center gap-2">
-                        <span class="material-icons text-white-500 text-3xl">home</span>
-                        <span class="hidden group-hover:inline">Dashboard</span>
-                    </a>
+                    <h2 class="menu-title">Usuario</h2>
+                    <ul>
+                        <li>
+                            <a href="/profile" class="flex items-center space-x-2 text-indigo-600 transition-colors rounded-lg group hover:bg-indigo-600 hover:text-white">
+                                <i class="material-icons text-3xl">person</i> Perfil</a>
+                        </li>
+                        <li>
+                            <a href="/logout" class="flex items-center space-x-2 text-indigo-600 transition-colors rounded-lg group hover:bg-indigo-600 hover:text-white">
+                                <i class="material-icons text-3xl">logout</i> Cerrar session</a>
+                        </li>
+                    </ul>
                 </li>
-                <li>
-                    <a href="/product" class="flex items-center gap-2">
-                        <span class="material-icons text-white-500 text-3xl">view_list</span>
-                        <span class="hidden group-hover:inline">Productos</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="/company" class="flex items-center gap-2">
-                        <span class="material-icons text-white-500 text-3xl">settings</span>
-                        <span class="hidden group-hover:inline">Configuración</span>
-                    </a>
-                </li>
-            </ul>
-        </div>
+                </ul>
+            </nav>
+        </aside>
+
+        <!-- Contenido principal -->
+        <div class="content flex-1" onclick="closeAllSidebars()">
