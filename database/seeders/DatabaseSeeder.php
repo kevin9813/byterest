8292002e8c6cc4fa887b29a26a1b2e62fd4bbe7a
domus_desarrollo2 <desarrollo2@domus.la>
2025-03-29 16:57:58 -0500

    session

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use App\Models\City;
+use App\Models\Company;
+use App\Models\Branch;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,13 +16,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // City::factory()->create([
-        //     'name' => 'Cali',
-        // ]); 
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'usuario' => 'kevin13',
-        //     'password' => '$2y$12$wWYkIN0fpQLnVFygCc2OkOKoE3zecKohKHZCxedjh2wejmswkkUU.',
-        // ]); 
+
+        City::factory()->create([
+            'name' => 'Cali',
+        ]); 
+
+        Company::factory()->create([
+            'name' => 'Compania 1 test',
+            'slogan' => 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit',
+            'nit' => '123',
+            'email' => 'email@email.com'
+        ]); 
+
+        Branch::factory()->create([
+            'company_id' => 1,
+            'name' => 'Compania 1 test',
+            'address' => 'address 1 # 1'
+        ]); 
+
+        User::factory()->create([
+            'name' => 'Test User',
+            'usuario' => 'kevin13',
+            'password' => '$2y$12$wWYkIN0fpQLnVFygCc2OkOKoE3zecKohKHZCxedjh2wejmswkkUU.',
+        ]); 
     }
 }
